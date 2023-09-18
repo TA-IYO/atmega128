369f184331100faf658fc9a6761821a0f5656a53
taiyo <130480657+TA-IYO@users.noreply.github.com>
2023-09-18 21:09:30 +0900

    Update main.c

--- [Project]cooling_fan_system/main.c
+++ [Project]cooling_fan_system/main.c
@@ -4,11 +4,13 @@
 #include <util/delay.h>
 #include <stdio.h>
 #include "UART0.h"
-#include "motor.h"
+#include "LCD.h"
 #include "interrupt.h"
 #include "adc.h"
-#include "LCD.h"
+#include "motor.h"
 #include "timer.h"
+#include "fan_status.h"
+#include "rot_status.h"
 
 volatile int run_state = 0;
 volatile int rot_state = 0;
@@ -16,29 +18,39 @@ volatile int tim_state = 0;
 
 int read;
 int rot_cnt = 0;
+int time_index;
+int fan_led;
 
 #define mask_adc 11110001
-
 #define EnableMotor1()           PORTE &= ~0x04
 #define DisableMotor1()          PORTE |= 0x04
 #define ForwardMotor1()          PORTE &= ~0x01
 #define BackwardMotor1()         PORTE |= 0x01
 #define SpeedMotor1(s)           OCR3B = s
 
+int fan_status(int read, int fan_led);
+int rot_status(int rot_state, int rot_led);
+void fan_activator(int read);
+void INIT_INT0(void);
+void INIT_INT1(void);
+void INIT_INT2(void);
+void LCD_IDLE(void);
+void LCD_RUN(void);
+
 FILE OUTPUT = FDEV_SETUP_STREAM(UART0_transmit, NULL, _FDEV_SETUP_WRITE);
 FILE INPUT = FDEV_SETUP_STREAM(NULL, UART0_receive, _FDEV_SETUP_READ);
 
 int main(void)
 {
-   DDRA = 0xFF;
-   DDRB = 0xFF;
-   PORTB = 0x00;
-   DDRC = 0xFF;
-   DDRD = 0x00;
-   PORTD = 0x00;
-   DDRE = 0x1D;
-   PORTE = 0x00;
-   DDRF = 0xFE;
+   DDRA =	0xFF;
+   DDRB =	0xFF;
+   PORTB =			0x00;
+   DDRC =	0xFF;
+   DDRD =	0x00;
+   PORTD =			0x00;
+   DDRE =	0x1D;
+   PORTE =			0x00;
+   DDRF =	0xFE;
 
    INIT_INT0();
    INIT_INT1();
@@ -56,40 +68,34 @@ int main(void)
       {
          if(run_state == 0)
          {
-            PORTB = 0x00;
+            LCD_IDLE();
+			PORTB = 0x00;
             rot_state = 0;
             tim_state = 0;
             time_index = -1;
             SpeedMotor1(0);
-            
-            LCD_Init();
-            LCD_wBCommand(0x80 | 0x00);
-            LCD_wString("Waiting to");
-            LCD_wBCommand(0x80 | 0x40);
-            LCD_wString("START");
+			
+printf("IDLE\r\n");
          }
          else
             break;
       }
       
-      PORTB = (PORTB | 0x01);
-
-      PORTB = rot_status(rot_state);
-
+printf("BTN 1 PUSHED\r\n");
+	  
       read = read_ADC();
+	  LCD_RUN();
       fan_activator(read);
-
-      LCD_Init();
-      LCD_wBCommand(0x80 | 0x00);
-      LCD_wString("FAN LEVEL : ");
-      LCD_wBCommand(0x80 | 0x0A);
+	  PORTB = (PORTB | 0x01);
+      PORTB = rot_status(rot_state, PORTB);
       
-      PORTB = fan_status(read, rot_state);
+printf("FAN is activated at %d power\r\n", read);
+
+      
+	  fan_led = PORTB;
+      PORTB = fan_status(read, fan_led);
       timer(&tim_state);
       rotator(rot_state, rot_cnt);
    }
    return 0;
 }
-
-
-
