c0c40f18d3a48b1007ede3001ea1986c954aa575
taiyo <130480657+TA-IYO@users.noreply.github.com>
2023-09-18 21:10:37 +0900

    Update rot_status.c

--- [Project]cooling_fan_system/rot_status.c
+++ [Project]cooling_fan_system/rot_status.c
@@ -1,20 +1,25 @@
+#define F_CPU 16000000UL
 #include "LCD.h"
 
 int rot_state;
-int rot_led = 0;
+int rot_led;
 
-int rot_status(int rot_state)
+int rot_status(int rot_state, int rot_led)
 {
     if(rot_state)
           {
              rot_led = (rot_led | 0x10);
              LCD_wBCommand(0x80 | 0x0C);
              LCD_wString("ROT");
+			 
           }
-          else
+		  
+	else
           {
              rot_led = (rot_led & 0xEF);
              LCD_wBCommand(0x80 | 0x0C);
-             LCD_wString("   ");
+             LCD_wString("   "); 
+			 
           }
+	 return rot_led;
 }
